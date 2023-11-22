c6cd0feaf7e3765e9bc2e85459de7fc06db26616
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-11-22 23:23:45 +0530

    Add files via upload

---
+++ JAVASCRIPT/Task2/script.js
@@ -0,0 +1,46 @@
+function add()
+{
+  var numOne, numTwo, sum;
+  numOne = parseInt(document.getElementById("first").value);
+  numTwo = parseInt(document.getElementById("second").value);
+  sum = numOne + numTwo;
+  document.getElementById("answer").value = sum;
+}
+numOne = parseInt(document.getElementById("first").value);
+document.getElementById("answer").value = sum;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function reset()
+{
+    document.getElementById("txtNum1").value = "0";
+    document.getElementById("txtNum2").value = "0";
+    document.getElementById("Sum").value = "0";
+}
+function add()
+{
+    alert(document.getElementById("txtNum1").value);
+    
+}*/
